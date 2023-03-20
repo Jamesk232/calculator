6c775aff21752f33b0b17d02ae426d620674ec52
James <jkaranja6202@gmail.com>
2023-03-20 16:45:20 -0400

    finished simple calculator

--- scripts.js
+++ scripts.js
@@ -1,40 +1,45 @@
 //all buttons
-const buttons = document.querySelectorAll("button");
 
-window.addEventListener("DOMContentLoaded", function(){
-	let result = [];
-	clickedBtn(result);
+const buttons = document.querySelectorAll("input[type='button']");
+
+// display screen
+const screen = document.querySelector("input[name='screen']");
+
+screen.value = "0";
+
+const calculator = () => {
+
+	
+	buttons.forEach((btn)=>{
+	btn.addEventListener("click", (e)=>{
+
+		if(e.currentTarget.value == 'C'){
+			screen.value = "0";
+		}else if(e.currentTarget.value == '='){
+			screen.value = eval(screen.value);
+		}else{
+
+			if(screen.value == '0'){
+
+				if(!(['=', '+', '-', '*', '/'].includes(e.currentTarget.value))){
+					screen.value = e.currentTarget.value;
+				}else{
+					screen.value += e.currentTarget.value;
+				}
+
+			}else{
+				screen.value += e.currentTarget.value;
+			}
+
+		}
+
+	})
 })
 
-//highlight buttons when clicked
-const clickedBtn = (rslt) => {
-	buttons.forEach((btn)=>{
-		btn.addEventListener("click", (e)=>{
-			//save values in result array
-			e.currentTarget.value != "=" ? (populateResult(rslt, e.currentTarget.value)) : (console.log(rslt), showResult(rslt));
-		})
-	})
 }
 
-//populate result
-const populateResult = (arr, val) => {
-	isNaN(Number(val)) ? arr.push(val) : arr.push(Number(val));
-}
 
-//show result of calculation
-const showResult = (arr) => {
-	let problem = "";
-	let numJoiner = [];
-	let symbol = "";
+calculator();
 
-	arr.map((val) => {
-		isNaN(Number(val)) ? 
-		(symbol = val,
-			problem += numJoiner.join(""),
-			numJoiner = []
-		) : numJoiner.push(Number(val));
-	})
 
-	console.log(numJoiner, symbol, problem)
-}
 
